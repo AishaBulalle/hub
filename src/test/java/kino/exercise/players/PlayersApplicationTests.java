e216c1828e2c6c2abcbf119467ba5eb87af2c15c
AishaBulalle <124045374+AishaBulalle@users.noreply.github.com>
2024-06-17 19:49:07 +0200

    Your commit message

---
+++ src/test/java/kino/exercise/players/PlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package kino.exercise.players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
